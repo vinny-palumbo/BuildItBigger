8a47bb24daf45d1630947dee3f79a027c86dbf4c
Vinny <vin_palumbo@hotmail.com>
2016-03-23 00:38:58 -0400

    Make the Java Library randomly select 1 of 10 jokes when button is pressed

--- javaJokes/src/main/java/com/udacity/gradle/jokes/Joker.java
+++ javaJokes/src/main/java/com/udacity/gradle/jokes/Joker.java
@@ -1,7 +1,25 @@
 package com.udacity.gradle.jokes;
 
+import java.util.Random;
+
 public class Joker {
+    String[] jokesArray = {
+        "Joke A",
+        "Joke B",
+        "Joke C",
+        "Joke D",
+        "Joke E",
+        "Joke F",
+        "Joke G",
+        "Joke H",
+        "Joke I",
+        "Joke J"
+    };
+
     public String getJoke(){
-        return "This is totally a funny joke";
+        Random random = new Random();
+        int index = random.nextInt(10);
+        return jokesArray[index];
     }
+
 }
