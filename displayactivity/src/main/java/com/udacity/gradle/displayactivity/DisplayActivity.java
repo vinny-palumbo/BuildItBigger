90acd421ed7ede13e20a028d98678d1e59edfc80
Vinny <vin_palumbo@hotmail.com>
2016-03-30 17:08:26 -0400

    Add loading indicator to custom Android Library's DisplayActivity

--- displayactivity/src/main/java/com/udacity/gradle/displayactivity/DisplayActivity.java
+++ displayactivity/src/main/java/com/udacity/gradle/displayactivity/DisplayActivity.java
@@ -2,9 +2,12 @@ package com.udacity.gradle.displayactivity;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ProgressBar;
 
 
 public class DisplayActivity extends ActionBarActivity {
+    private ProgressBar spinner;
 
     public static String JOKE_KEY = "Joke key";
 
@@ -12,6 +15,10 @@ public class DisplayActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        spinner=(ProgressBar)findViewById(R.id.progressBar);
+        spinner.setVisibility(View.VISIBLE);
+
     }
 
 }
