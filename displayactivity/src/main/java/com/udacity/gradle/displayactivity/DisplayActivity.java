136034074cdf81fc0437639fe12cc067186cc74e
Vinny <vin_palumbo@hotmail.com>
2016-03-30 17:38:42 -0400

    Hide Loading Indicator after Joke has been retrieved

--- displayactivity/src/main/java/com/udacity/gradle/displayactivity/DisplayActivity.java
+++ displayactivity/src/main/java/com/udacity/gradle/displayactivity/DisplayActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
 public class DisplayActivity extends ActionBarActivity {
@@ -18,6 +19,11 @@ public class DisplayActivity extends ActionBarActivity {
 
         spinner=(ProgressBar)findViewById(R.id.progressBar);
         spinner.setVisibility(View.VISIBLE);
+
+        TextView jokeTextView = (TextView) findViewById(R.id.displayJokeTextView);
+        if(!jokeTextView.equals(null)){
+            spinner.setVisibility(View.GONE);
+        }
 
     }
 
