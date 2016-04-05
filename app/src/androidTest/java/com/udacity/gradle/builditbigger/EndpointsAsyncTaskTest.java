e12428032efb2a936d649ef7798e3b52d99ee772
Vinny <vin_palumbo@hotmail.com>
2016-04-04 23:58:41 -0400

    Update the AsyncTask Android Test to use .get() to block the thread in order to wait for the content to load

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -2,12 +2,22 @@ package com.udacity.gradle.builditbigger;
 
 import android.test.AndroidTestCase;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class EndpointsAsyncTaskTest extends AndroidTestCase {
     public void testNonEmptyString() {
-        assertNotNull(new EndpointsAsyncTask().execute(getContext()));
-        assertNotSame(new EndpointsAsyncTask().execute(getContext()), "");
+        try {
+            EndpointsAsyncTask jokeTask = new EndpointsAsyncTask();
+            jokeTask.execute(getContext());
+            String joke = jokeTask.get(30, TimeUnit.SECONDS);
+
+            assertNotNull(joke);
+            assertNotSame(joke, "");
+        } catch (Exception e){
+            fail("Timed out");
+        }
     }
 }
